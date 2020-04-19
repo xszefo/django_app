819c2d04fd4461c35efb179ccf5c9542fff6a952
xszefo <piotr.sumelka@gmail.com>
2020-04-19 10:52:36 +0200

    Tutorial #2

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+def products(request):
+    return HttpResponse('Products')
+
+def customer(request):
+    return HttpResponse('Customer')
