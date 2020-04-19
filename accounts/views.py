fa42618e02808150eb1a07b168c57e13ffcc9f06
xszefo <piotr.sumelka@gmail.com>
2020-04-19 11:32:16 +0200

    Tutorial #3

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('Products')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('Customer')
+    return render(request, 'accounts/customer.html')
