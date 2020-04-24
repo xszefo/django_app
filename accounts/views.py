59caefdf54c025affe12eb62198380125e035651
xszefo <piotr.sumelka@gmail.com>
2020-04-24 19:12:09 +0200

    Tutorial #14

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,57 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import inlineformset_factory
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    context = {}
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    context ['form'] = form
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -25,6 +71,7 @@ def home(request):
             }
     return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     context = {}
@@ -32,6 +79,7 @@ def products(request):
     context['products'] = products
     return render(request, 'accounts/products.html', context)
 
+@login_required(login_url='login')
 def customer(request, pk=None):
     customer = get_object_or_404(Customer, pk=pk)
     orders = customer.order_set.all()
@@ -48,6 +96,7 @@ def customer(request, pk=None):
             }
     return render(request, 'accounts/customer.html', context)
 
+@login_required(login_url='login')
 def createOrder(request, pk):
     context = {}
     
@@ -67,6 +116,7 @@ def createOrder(request, pk):
     return render(request, 'accounts/order_form.html', context)
 
 
+@login_required(login_url='login')
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -83,6 +133,7 @@ def updateOrder(request, pk):
 
     return render(request, 'accounts/update_order.html', context)
 
+@login_required(login_url='login')
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
 
